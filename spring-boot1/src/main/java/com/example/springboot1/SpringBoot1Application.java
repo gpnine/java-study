6d15d2a994f358ba50cf0641ad0b8a568c03f47f
朱成琳 <zhuchenglin@homolo.com>
2019-07-15 15:54:57 +0800

    spring4all增强对mongo的配置

--- spring-boot1/src/main/java/com/example/springboot1/SpringBoot1Application.java
+++ spring-boot1/src/main/java/com/example/springboot1/SpringBoot1Application.java
@@ -1,8 +1,10 @@
 package com.example.springboot1;
 
+import com.spring4all.mongodb.EnableMongoPlus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableMongoPlus
 @SpringBootApplication
 public class SpringBoot1Application {
 
